85f3360e15ed3ce6fe7fda72dcd8496a88277cd0
404kevnotfound <student@CSiMac1-6.local>
2019-03-04 13:16:00 -0700

    Program To Calculate Area Of A Rectangle

---
+++ Programming Exercise 1-9/Programming_exercise_1_9.java
@@ -0,0 +1,18 @@
+/*
+* - Brent Dalling
+* - March 25th, 2019
+*
+* - Computes An Equation And Displays The Console
+*/
+
+class Untitled {
+	public static void main(String[] args) {
+		
+		// Double Var To Hold Decimals
+		// area = width * height;
+		double area = 4.5 * 7.9;
+		
+		// Display Output To Console
+		System.out.println(area);
+	}
+}
