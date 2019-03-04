581717da0d9a55b676e6f7a0938884a899177273
404kevnotfound <student@CSiMac1-6.local>
2019-03-04 14:05:43 -0700

    Chapter 1 Assignments

---
+++ chapter 1/Programming Exersize 1-5/Programming_exercise_1_5.java
@@ -0,0 +1,14 @@
+/*
+* - Brent Dalling
+* - March 25th, 2019
+*
+* - Computes An Equation And Displays The Console
+*/
+
+class Untitled {
+	public static void main(String[] args) {
+		System.out.println(
+			((9.5 * 4.5) - (2.5 * 3) / (45.5 - 3.5))
+		);
+	}
+}/
