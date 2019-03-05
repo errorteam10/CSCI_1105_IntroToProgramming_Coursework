140526b6ce7f43e0934e1ffd200d7633d1548fcb
404kevnotfound <student@CSiMac1-6.local>
2019-03-05 12:32:31 -0700

    Redid programming exercise 4-17

--- Chapter 4/programming_exercise_4_17.java
+++ Chapter 4/programming_exercise_4_17.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 /*
 * - Brent Dalling
@@ -6,62 +7,45 @@
 * - Takes User Input To Determine Days In The Month
 */
 
-// Importing Tools
 import java.util.Scanner;
-import java.time.YearMonth;
-import java.util.Arrays;
+import java.time.*;
 
 class programming_exercise_4_17 {
 	public static void main(String[] args) {
 		
 		// Define Variables 
-		int 		year;
-		String 		month;
-		YearMonth 	yearMonth;
-		int 		selectedMonth;
-		Scanner 	user = new Scanner(System.in);
+		boolean leap;
+		String 	month;
+		int 	year;
+		Scanner user = new Scanner(System.in);
+		Calendar calendar = new GregorianCalendar();
 		
-		// Define Months
-		String[] months = {
-			"Jan", 
-			"Feb", 
-			"mar",
-			"apr",
-			"may", 
-			"jun", 
-			"jul",
-			"aug", 
-			"sep", 
-			"oct", 
-			"nov",
-			"dec"
-		};
-		
-		// get user input for year
-		System.out.print("Enter Year: ");
+		// get year from the user 
+		System.out.println("Enter Year: ");
 		year = user.nextInt();
 		
-		// get user input for month
-		System.out.print("Enter 3 Char Month: ");
+		// get the month from the user
+		System.out.println("Enter Month: ");
 		month = user.next();
 		
-		// Determine Month Code
-		selectedMonth = Arrays.asList(months).indexOf(month);
-		
-		// Determine If Negative Conversion Is Required
-		if(selectedMonth < 0) {
+		// determine days in month
+		if(month.equals("jan") || month.equals("mar") || month.equals("may") || month.equals("jul") || month.equals("aug") || month.equals("oct") || month.equals("dec")) {
 			
-			// Define The YearMonth Object		
-			yearMonth = YearMonth.of(year, (selectedMonth * -1) + 1);
-
+			// Display Amount Of Days In Month
+			System.out.println(month + " has 31 days.");
+			
+		} else if(month.equals("apr") || month.equals("jun") || month.equals("sep") || month.equals("nov")) {
+			
+			// Display Amount Of Days In Month
+			System.out.println(month + " has 30 days.");
+			
+		} else if(month.equals("feb")) {
+			
+			// Determine If Leap Year And Then Display To User
+			System.out.println(month + " has " + (year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ? "29" : "28") + " days.");
 		} else {
-			
-			// Define The YearMonth Object		
-			yearMonth = YearMonth.of(year, selectedMonth + 1);
-
+			System.out.println(month + " Is Not A Valid Month.");
 		}
-				
-		// Display To User Days In The Month
-		System.out.println(yearMonth.lengthOfMonth());
+		
 	}
 }
